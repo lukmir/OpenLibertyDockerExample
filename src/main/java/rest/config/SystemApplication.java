1ece11aa6c416eb762b4a06a0b2d6a61663cd83f
lukmir <lukmir2@gmail.com>
2018-03-20 18:33:48 +0100

    Added REST Application path

---
+++ src/main/java/rest/config/SystemApplication.java
@@ -0,0 +1,8 @@
+package rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("System")
+public class SystemApplication extends Application {
+}
